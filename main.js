bf74b45702ac3ac431a10d3b509728356f0a1314
Ben Hensor <benhensor@gmail.com>
2023-03-16 16:04:10 +0000

    comments added and tasks 4 and 5

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// TASK 1
+
 const favouriteSongs = ["Tiny Dancer", "Running Up That Hill", "Don't Stop Moving", "Tragedy", "Crazy Horses"];
 
 const pyramidScheme = [1, 2, 4, 8, 16, 32, 64];
@@ -16,6 +18,8 @@ const friends = [
 ]
 
 console.log(friends[2].skill);
+
+// TASK 2
 
 let options = [
   "Jan",
@@ -37,9 +41,27 @@ console.log(options[8]);
 console.log(options[3]);
 console.log(options[11]);
 
+// TASK 3
+
+/*
 let englishNumbers = ["one", "two", "three", "four", "five"];
 let i=0
 while (i != englishNumbers.length){
 console.log(englishNumbers[i]);
 i++;
+}
+*/
+// TASK 4
+
+let englishNumbers = ["one", "two", "three", "four", "five"];
+for (i = 0; i < 5; i++) {
+  console.log(englishNumbers[i]);
+}
+
+// TASK 5
+
+let words = ["ben", "ha", "spla", "fa", "ca", "dra"];
+// for loop goes here
+for (i = 0; i < 6; i++) {
+console.log(words[i] + "t");
 }
