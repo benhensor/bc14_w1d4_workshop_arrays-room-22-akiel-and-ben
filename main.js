f7edb20c90a7b9ac6a1208267781fd385f3ce267
AkielCh <akiel98@hotmail.com>
2023-03-16 17:20:18 +0000

    Work from today

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // TASK 1
 
-const favouriteSongs = ["Tiny Dancer", "Running Up That Hill", "Don't Stop Moving", "Tragedy", "Crazy Horses"];
+/*const favouriteSongs = ["Tiny Dancer", "Running Up That Hill", "Don't Stop Moving", "Tragedy", "Crazy Horses"];
 
 const pyramidScheme = [1, 2, 4, 8, 16, 32, 64];
 
@@ -20,7 +20,7 @@ const friends = [
 console.log(friends[2].skill);
 
 // TASK 2
-
+/*
 let options = [
   "Jan",
   "Feb",
@@ -40,7 +40,7 @@ console.log(options[8]);
 
 console.log(options[3]);
 console.log(options[11]);
-
+*/
 // TASK 3
 
 /*
@@ -51,17 +51,47 @@ console.log(englishNumbers[i]);
 i++;
 }
 */
+
+
+
 // TASK 4
+
+/*
 
 let englishNumbers = ["one", "two", "three", "four", "five"];
 for (i = 0; i < 5; i++) {
   console.log(englishNumbers[i]);
 }
 
-// TASK 5
 
+// TASK 5
+/*
 let words = ["ben", "ha", "spla", "fa", "ca", "dra"];
 // for loop goes here
 for (i = 0; i < 6; i++) {
 console.log(words[i] + "t");
-}
+}*/
+
+
+
+/*
+ let diary=[
+["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"],
+["Week1","Week2","Week3","Week4"],
+["Month1","month2"]
+ ]
+
+
+
+console.log(diary[0][1],diary[1][2],diary[0][0]);*/
+
+let place =[
+{Name:"Home",type:"House",Material:"Brick"},
+{Name:"Wales",type:"Tent", Material:"Nylon"},
+{Name:"Lake District",type:"Cabin", Material:"Wood"},
+{Name:"Wild",type:"Hut", Material:"Mud"},
+
+]
+
+let selection =prompt(`select number 0-3`);
+console.log(place[selection].type, " made of", place[selection].Material  );
