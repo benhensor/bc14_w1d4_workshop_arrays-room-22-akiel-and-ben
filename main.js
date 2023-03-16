8ab8179db12bbfb682f25fd1abe54ec8bb48894e
Ben Hensor <benhensor@gmail.com>
2023-03-16 15:39:42 +0000

    files added, task 1 complete

---
+++ main.js
@@ -0,0 +1,19 @@
+const favouriteSongs = ["Tiny Dancer", "Running Up That Hill", "Don't Stop Moving", "Tragedy", "Crazy Horses"];
+
+const pyramidScheme = [1, 2, 4, 8, 16, 32, 64];
+
+const friends = [
+  {name: 'chris', skill: 'changing lives'},
+  {name: 'lizzie', skill: 'being friendly'},
+  {name: 'ben', skill: 'code rapping'},
+  {name: 'tao', skill: 'hand raising'},
+  {name: 'liz', skill: 'crochet'},
+  {name: 'patrick', skill: 'Call of Duty'},
+  {name: 'tim', skill: 'analogies'},
+  {name: 'james', skill: 'friendship'},
+  {name: 'joseph', skill: 'being positive'},
+  {name: 'max', skill: 'papier-mâché'},
+]
+
+console.log(friends[2].skill);
+
